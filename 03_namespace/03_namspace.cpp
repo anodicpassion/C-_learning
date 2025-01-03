d077d91e258d6603ce5138c6ed27e5d4596d332c
anodicpassion <pratikpawarkarad@gmail.com>
2025-01-04 00:19:35 +0530

    complete namespace

---
+++ 03_namespace/03_namspace.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main() {
+    // using whole standar library in namespace
+    using namespace std;
+
+    // using cout without scope resolution operator (std::cout)
+    cout << "hello_world" << endl;
+    return 0;
+}
---
+++ 03_namespace/04_namespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // using namespace for only cout (not for other packages in stander library
+    using std::cout;
+    using std::endl;
+
+    cout << "hello_world" << endl;
+    return 0;
+}
