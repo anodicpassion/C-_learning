286d524571393e94b45bf380f2d5b01eaa7b983d
anodicpassion <pratikpawarkarad@gmail.com>
2025-01-03 23:55:22 +0530

    added const

--- 02_datatypes/circumference_of_circle.cpp
+++ 02_datatypes/circumference_of_circle.cpp
@@ -2,14 +2,14 @@
 
 int main() {
     
-    // defining the pi 
-    double pi = 3.14159;
+    // defining the pi as const
+    const double PI = 3.14159;
     
     // defining the radius of the circle
     double radius = 10;
 
     // getting into formula
-    double circumference = 2 * pi * radius;
+    double circumference = 2 * PI * radius;
 
     std::cout << "Circumference of the circle with radius (" << radius << ") is: " << circumference << std::endl;
     return 0;
