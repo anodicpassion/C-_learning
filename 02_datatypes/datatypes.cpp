62ea86b23d583a5140d132d4fdc90e36afa0b69f
anodicpassion <pratikpawarkarad@gmail.com>
2025-01-03 23:46:27 +0530

    learned datatypes

---
+++ 02_datatypes/datatypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    // declaring the integer
+    int a;
+    a = 5;
+
+    // declaring the double (floating points)
+    double b = 5.5;
+
+    // single character 
+    char s = 'b';
+
+    // bool (true or false)
+    bool k = true;
+
+    // string (object of sequence of chars)
+    std::string name = "Hi Bro";
+
+    std::cout << "int a: " << a << std::endl << "double b: " << b << std::endl << "char s: " << s << std::endl;
+    std::cout << "bool k: " << k << std::endl << "std::string name: " << name << std::endl;
+    return 0;
+}
